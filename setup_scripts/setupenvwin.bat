5875b682a5925285a2847d8a534e6db3be09973e
phantazeeer <shturmovik729@gmail.com>
2024-12-21 16:07:41 +0400

    added env create script

---
+++ setup_scripts/setupenvwin.bat
@@ -0,0 +1,23 @@
+SET venv=.venv\Scripts
+SET source=setup_scripts
+cd ..
+IF NOT EXIST ".venv" (
+    python -m venv .venv
+    %venv%\pip.exe install -r requirements.txt
+) ELSE (
+    ECHO 'Delete python venv for installing correctly'
+)
+
+IF NOT EXIST ".env" (
+    IF NOT EXIST ".env.template" (
+        ECHO ".env.template is not exist"
+    ) ELSE (
+        COPY .env.template .env
+    )
+)
+
+IF NOT EXIST ".git" (
+    ECHO "git repository does not exist"
+) ELSE (
+    COPY %source%\pre-commit .git\hooks\pre-commit
+)
