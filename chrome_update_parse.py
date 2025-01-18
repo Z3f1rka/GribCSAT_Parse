63b2f2c80685ccce762ae47f4e907b0f9e5dbcac
phantazeeer <shturmovik729@gmail.com>
2025-01-18 21:53:26 +0400

    config.yml cleaned config

--- chrome_update_parse.py
+++ chrome_update_parse.py
@@ -4,7 +4,8 @@
 import requests
 
 if __name__ == "__main__":
-    c = requests.get("https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json").json()
+    c = requests.get(
+        "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json").json()
     link = ""
     for i in c["channels"]["Stable"]["downloads"]["chromedriver"]:
         if i["platform"] == 'linux64':
