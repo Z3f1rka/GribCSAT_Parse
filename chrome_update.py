64b5b5755acfa66d8c0017446528bd27ec2c0c8a
phantazeeer <shturmovik729@gmail.com>
2025-01-18 21:43:09 +0400

    config.yml cleaned config

---
+++ chrome_update.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# flake8: noqa
+
+import requests
+
+if __name__ == "__main__":
+    c = requests.get("https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json").json()
+    link = ""
+    for i in c["channels"]["Stable"]["downloads"]["chromedriver"]:
+        if i["platform"] == 'linux64':
+            link = i["url"]
+    print(link)
