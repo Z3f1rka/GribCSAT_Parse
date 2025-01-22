cd8591e2dc735b9bef1732da4876df37b88b5ecf
phantazeeer <shturmovik729@gmail.com>
2025-01-22 23:50:02 +0400

    added pydantic output validator

--- app/routes/find_by_link.py
+++ app/routes/find_by_link.py
@@ -2,15 +2,16 @@ from fastapi import APIRouter
 
 from app.driver.driver_session import get_driver
 from app.services import ServiceDriver
+from app.validation_models import FBLOUTPUT
 
 router = APIRouter()
 
 
 @router.get("/find_by_link/")
-def find_info(address: str, count: int = 0) -> dict:
+def find_info(address: str, count: int = 0) -> FBLOUTPUT:
     driver = get_driver()
     product = ServiceDriver.find_by_link(driver, address, count)
-    return {"product": product}
+    return product
 
 
 if __name__ == "__main__":
